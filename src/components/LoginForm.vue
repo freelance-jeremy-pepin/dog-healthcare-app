cb409aa8f700d1bd3941fa4389e66356340d2d2f
Jérémy <pepin.jeremy81@gmail.com>
2020-06-03 12:34:47 +0200

    ajout du composant LoginForm

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,21 @@
+<template>
+  <q-form>
+    <q-input label='email' type='email' v-model='email'></q-input>
+    <q-input label='password' type='password' v-model='password'></q-input>
+  </q-form>
+</template>
+
+<script lang='ts'>
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class LoginForm extends Vue {
+  private email = '';
+
+  private password = '';
+}
+</script>
+
+<style scoped>
+
+</style>

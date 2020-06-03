8fc2cb6af70351c630ea5a25b973ef7c86816b72
Jérémy <pepin.jeremy81@gmail.com>
2020-06-03 13:01:11 +0200

    mise en place de la page de Login

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,8 +1,34 @@
 <template>
-  <q-form>
-    <q-input label='email' type='email' v-model='email'></q-input>
-    <q-input label='password' type='password' v-model='password'></q-input>
-  </q-form>
+  <q-page class="bg-grey-green window-height window-width row justify-center items-center">
+    <div class="column">
+      <div class="row">
+        <h5 class="text-h5 text-accent q-my-md">Dog Healthcare</h5>
+      </div>
+      <div class="row">
+        <q-card bordered class="q-pa-lg shadow-1" square>
+          <q-card-section>
+            <q-form class="q-gutter-md">
+              <q-input clearable filled label="email" square type="email" v-model="email" />
+              <q-input
+                clearable
+                filled
+                label="password"
+                square
+                type="password"
+                v-model="password"
+              />
+            </q-form>
+          </q-card-section>
+          <q-card-actions class="q-px-md">
+            <q-btn class="full-width" color="light-green-7" label="Login" size="lg" unelevated />
+          </q-card-actions>
+          <q-card-section class="text-center q-pa-none">
+            <p class="text-grey-6">Not registered? Created an Account</p>
+          </q-card-section>
+        </q-card>
+      </div>
+    </div>
+  </q-page>
 </template>
 
 <script lang='ts'>
@@ -17,5 +43,7 @@ export default class LoginForm extends Vue {
 </script>
 
 <style scoped>
-
+  .q-card {
+    width: 360px;
+  }
 </style>
---
+++ src/pages/Login.vue
@@ -0,0 +1,19 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-page-container>
+      <login-form></login-form>
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import LoginForm from 'components/LoginForm.vue';
+
+@Component({
+  components: { LoginForm },
+})
+export default class Index extends Vue {
+
+}
+</script>
